c3edf8b7aacca0d1c1929f88d73411c04aaf4fb0
AkashKonda <kgovardhan@enphaseenergy.com>
2022-10-09 21:45:07 +0530

    1. Cread read write threads 2. Add a mutex access lock between both the read and write threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-extern crate queues;
-use queues;
 use serialport::available_ports;
 use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
+
 pub fn list_serial_ports() -> String {
     let ports = available_ports().unwrap();
     let mut index = 0;
@@ -24,34 +25,51 @@ pub fn list_serial_ports() -> String {
     return selected_port;
 }
 
-pub fn read_from_port(port_name: String) {
-    let open_port = serialport::new(port_name, 115200)
+fn main() {
+    println!("Hello, world!");
+    let selected_port = list_serial_ports();
+    println!("Selected port: {:?}", selected_port);
+    let open_port = serialport::new(selected_port, 115200)
         .timeout(Duration::from_millis(10))
-        .open();
+        .open()
+        .unwrap();
 
-    match open_port {
-        Ok(mut port) => {
-            let mut serial_buf: Vec<u8> = vec![0;1000];
-            println!("Connection successfully established\n");
-            loop {
-                match port.read(serial_buf.as_mut_slice()) {
+    let port_access = Arc::new(Mutex::new(open_port));
+
+    let read_mutex = Arc::clone(&port_access);
+    let read_thread = thread::spawn(move || {
+        let mut serial_buf: Vec<u8> = vec![0; 1000];
+        println!("Connection successfully established\n");
+        loop {
+            {
+                let mut local_port = read_mutex.lock().unwrap();
+                match (*local_port).read(serial_buf.as_mut_slice()) {
                     Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
                     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
                     Err(e) => eprintln!("{:?}", e),
                 }
             }
+            thread::sleep(Duration::from_millis(10));
         }
-        Err(e) => {
-            println!("Failed to open port with Error: {:?}", e);
+    });
+
+    let write_mutex = Arc::clone(&port_access);
+    let write_thread = thread::spawn(move || loop {
+        let mut std_input = String::new();
+        io::stdin()
+            .read_line(&mut std_input)
+            .expect("Std input not working\n");
+        println!("Input from user: {}", std_input);
+        {
+            let mut local_port = write_mutex.lock().unwrap();
+            println!("Mutex lock acquired\n");
+            match (*local_port).write(std_input.as_bytes()) {
+                Err(e) => println!("failed due to error {:?}", e),
+                Ok(n) => println!("Bytes written {}", n),
+            };
         }
-    }
+    });
 
-}
-
-
-fn main() {
-    println!("Hello, world!");
-    let selected_port = list_serial_ports();
-    println!("Selected port: {:?}", selected_port);
-    read_from_port(selected_port);
+    read_thread.join().unwrap();
+    write_thread.join().unwrap();
 }
